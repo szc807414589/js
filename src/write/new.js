b4728b4d1a08dab7635ff4a0eb8d696902a30783
szc807414589 <807414589@qq.com>
2020-03-05 23:37:29 +0800

    实现bind

---
+++ src/write/new.js
@@ -0,0 +1,41 @@
+/*
+* new 操作:
+*
+*   访问到 Person 构造函数里的属性
+*   访问到 Person.prototype 中的属性
+*
+*   实现:
+*           首先创建一个对象,
+*           再把这个对象绑定到原型链上,
+*           这个对象可以访问到构造函数原型上的属性,
+*
+*
+* */
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+Person.prototype.occupation = '学生';
+
+Person.prototype.sayYourName = function () {
+	console.log('我的名字是 ' + this.name);
+};
+let one = new Person('小明', 10);
+console.log(one);  //{name: "小明", age: 10}
+console.log(one.occupation); // 学生
+console.log(one.sayYourName()); //我的名字是小明
+console.log('---------------------------1---------------------------');
+
+function _New(fn, ...args) {
+	// let obj1 = {};
+	// Object.setPrototypeOf(obj, fn.prototype);
+	let obj = Object.create(fn.prototype);
+	let ret = fn.apply(obj, args);
+	return ret instanceof Object ? ret : obj;
+}
+
+let two = _New(Person, '嘻嘻', 24);
+console.log(two);
+console.log(two.occupation);
+console.log(two.sayYourName());
